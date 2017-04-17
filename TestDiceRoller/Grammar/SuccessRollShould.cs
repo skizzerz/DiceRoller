e04710ea9ed86137b5fb5c96afe82c4e39a0f5ac
Ryan Schmidt <skizzerz@skizzerz.net>
2017-04-17 16:10:17 -0700

    Additional tests, pending 1.0.0 release very soon

--- TestDiceRoller/Grammar/SuccessRollShould.cs
+++ TestDiceRoller/Grammar/SuccessRollShould.cs
@@ -36,6 +36,18 @@ namespace TestDiceRoller.Grammar
         }
 
         [TestMethod]
+        public void Successfully_SuccessExtra_Group()
+        {
+            EvaluateRoll("{2d6, 2d6}>7", SuccessConf, 4, "{2d6, 2d6}.success(>7) => ($9 + 7!) => 1 success");
+        }
+
+        [TestMethod]
+        public void Successfully_SuccessFunction_Group()
+        {
+            EvaluateRoll("{2d6, 2d6}.success(>7)", SuccessConf, 4, "{2d6, 2d6}.success(>7) => ($9 + 7!) => 1 success");
+        }
+
+        [TestMethod]
         public void Successfully_SuccessFailureExtra()
         {
             EvaluateRoll("4d6>=5f1", SuccessConf, 4, "4d6.success(>=5).failure(=1) => 4 + $5 + #1! + $6! => 1 success");
@@ -57,6 +69,18 @@ namespace TestDiceRoller.Grammar
         public void Successfully_SuccessFailureFunction_Fudge()
         {
             EvaluateRoll("4dF.success(=1).failure(=-1)", FudgeConf, 4, "4dF.success(=1).failure(=-1) => #-1! + 0 + $1! + $1! => 1 success");
+        }
+
+        [TestMethod]
+        public void Successfully_SuccessFailureExtra_Group()
+        {
+            EvaluateRoll("{2d6, 2d6}>7f<7", SuccessConf, 4, "{2d6, 2d6}.success(>7).failure(<7) => ($9 + 7!) => 1 success");
+        }
+
+        [TestMethod]
+        public void Successfully_SuccessFailureFunction_Group()
+        {
+            EvaluateRoll("{2d6, 2d6}.success(>7).failure(<7)", SuccessConf, 4, "{2d6, 2d6}.success(>7).failure(<7) => ($9 + 7!) => 1 success");
         }
 
         [TestMethod]
@@ -87,6 +111,24 @@ namespace TestDiceRoller.Grammar
         public void Successfully_AddSuccessToRoll_ResultTotal()
         {
             EvaluateRoll("2d6>=5 + 2d6", SuccessConf, 4, "2d6.success(>=5) + 2d6 => 4 + $5 + 1! + 6! => 8");
+        }
+
+        [TestMethod]
+        public void Successfully_AllGroupSuccess_ResultSuccess()
+        {
+            EvaluateRoll("{2d6>=5, 2d6>=5}", SuccessConf, 4, "{2d6.success(>=5), 2d6.success(>=5)} => (1 + 1!) => 2 successes");
+        }
+
+        [TestMethod]
+        public void Successfully_MixedGroupSuccessLiteral_ResultSuccess()
+        {
+            EvaluateRoll("{4d6>=5, 2}", SuccessConf, 4, "{4d6.success(>=5), 2} => (2! + 2) => 4 successes");
+        }
+
+        [TestMethod]
+        public void Successfully_MixedGroupSuccessRoll_ResultTotal()
+        {
+            EvaluateRoll("{2d6>=5, 2d6}", SuccessConf, 4, "{2d6.success(>=5), 2d6} => (1 + 7!) => 8");
         }
     }
 }
