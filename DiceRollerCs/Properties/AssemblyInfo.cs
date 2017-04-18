ff1adca9057a008247e2ed2a819fd08123004389
Ryan Schmidt <skizzerz@skizzerz.net>
2017-04-17 19:58:21 -0700

    Add nuget stuff

--- DiceRollerCs/Properties/AssemblyInfo.cs
+++ DiceRollerCs/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DiceRollerCs")]
-[assembly: AssemblyDescription("Dice roller for C#")]
+[assembly: AssemblyTitle("DiceRoller")]
+[assembly: AssemblyDescription("Extensible and well-documented dice roller with a robust set of features. Excellent for play-by-post forums, virtual tabletops, or for integrating into character sheets or encounter managers.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DiceRollerCs")]
-[assembly: AssemblyCopyright("Copyright © Ryan Schmidt 2017. Released under the MIT License.")]
+[assembly: AssemblyCompany("Ryan Schmidt")]
+[assembly: AssemblyProduct("DiceRoller")]
+[assembly: AssemblyCopyright("Copyright ©2017 Ryan Schmidt. Released under the MIT License.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("TestDiceRoller, PublicKey=00240000048000009400000006020000002400005253413100040000010001003792a563db33a3fba0b1a527d41e97bf49423541959d91347d298351e16d5f660d93a8d905b5e7edf32e9a075c1a62a67b9e2a46f95e240f5be8c478f7dd308eef93fe79e6042b2427aaf5c553d071e926d7dd2be7b7c9e6e504627237d200010798198dca4fcc6a52167412bd97fda5996239e58a12489eb079c24f2483069b")]
@@ -35,5 +35,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
+// IMPORTANT: When updating, also update DiceRollerCs.nuspec
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
