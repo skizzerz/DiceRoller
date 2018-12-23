f6d81887ac1947e907d3f6f3b7c2cdfc2897f2ef
Ryan Schmidt <skizzerz@skizzerz.net>
2018-12-23 12:14:44 -0700

    Put release script in a better spot

--- release.ps1
+++ release.ps1
@@ -1,6 +1,8 @@
-& "C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a bin\Release\net452\Dice.dll
-& "C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a bin\Release\netstandard2.0\Dice.dll
-D:\Programs\nuget.exe pack DiceRoller.csproj
+# Remember to build in release mode before running this script!
+# Note to self: grab pin for signing out of lastpass
+& "C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a DiceRoller\bin\Release\net452\Dice.dll
+& "C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a DiceRoller\bin\Release\netstandard2.0\Dice.dll
+D:\Programs\nuget.exe pack DiceRoller\Dice.csproj
 $vers = Get-ChildItem DiceRoller.*.nupkg | ForEach-Object { New-Object System.Version($_.Name.Substring(11, $_.Name.Length - 17)) } | Sort-Object -Descending
 $pkg = "DiceRoller.$($vers[0].Major).$($vers[0].Minor).$($vers[0].Build).nupkg"
 # Publish to NuGet; api key must be saved via nuget setApiKey beforehand
