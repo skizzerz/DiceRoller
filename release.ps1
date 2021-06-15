4f9e55e08fb9b8d922e50ad3da62f61c8b793571
Ryan Schmidt <skizzerz@skizzerz.net>
2021-06-14 20:18:02 -0700

    Update release script

--- release.ps1
+++ release.ps1
@@ -1,8 +1,8 @@
-# Remember to build in release mode before running this script!
-# Note to self: grab pin for signing out of lastpass
-& "C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a DiceRoller\bin\Release\net452\Dice.dll
-& "C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a DiceRoller\bin\Release\netstandard2.0\Dice.dll
-& "C:\Program Files\dotnet\dotnet.exe" pack DiceRoller\Dice.csproj --configuration Release --no-build --output . --include-symbols -p:SymbolPackageFormat=snupkg
+& "C:\Program Files\dotnet\dotnet.exe" clean DiceRoller\Dice.csproj --configuration Release --nologo
+& "C:\Program Files\dotnet\dotnet.exe" build DiceRoller\Dice.csproj --configuration Release --nologo
+& "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a DiceRoller\bin\Release\net452\Dice.dll
+& "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\signtool.exe" sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a DiceRoller\bin\Release\netstandard2.0\Dice.dll
+& "C:\Program Files\dotnet\dotnet.exe" pack DiceRoller\Dice.csproj --configuration Release --no-build --output . --include-symbols -p:SymbolPackageFormat=snupkg --nologo
 $vers = Get-ChildItem DiceRoller.*.nupkg | ForEach-Object { New-Object System.Version($_.Name.Substring(11, $_.Name.Length - 17)) } | Sort-Object -Descending
 $pkg = "DiceRoller.$($vers[0].Major).$($vers[0].Minor).$($vers[0].Build).nupkg"
 # Publish to NuGet; api key must be saved via nuget setApiKey beforehand
